94ed903289af1d16e452eca31dac2063d73a657c
MartinBoeckling <m_boeck@outlook.de>
2022-05-26 12:13:27 +0000

    Updated RDF2Vec xgboost

--- wildfirearea/modelling/extragboost.py
+++ wildfirearea/modelling/extragboost.py
@@ -25,8 +25,8 @@ from pathlib import Path
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import classification_report
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix, roc_auc_score
 from skopt import BayesSearchCV
 from skopt.space.space import Real, Categorical, Integer
 from imblearn.over_sampling import RandomOverSampler
@@ -34,125 +34,171 @@ import pickle
 import xgboost as xgb
 
 
-def dataPreprocess(dataPath, testDate):
-    print('Data Preprocessing')
-    # transform datafile into pathlib object
-    dataPath = Path(dataPath)
-    # check if input is eligeble for data processing
-    # check if dataPath input is a file
-    assert dataPath.is_file(), f'{dataPath} does not link to a file'
-    # check if testDate input can be interpreted as date
-    assert not pd.isna(pd.to_datetime(testDate, errors='coerce')), f'{testDate} is not an eligible date'
-    # read file into dataframe
-    data = pd.read_csv(dataPath)
-    # transform DATE column into datetime
-    data['DATE'] = pd.to_datetime(data['DATE'])
-    # split data into train and testset based on specified date
-    # create train dataframe which is under specified date
-    trainData = data[data['DATE'] < testDate]
-    # create test dataframe which is over specified date
-    testData = data[data['DATE'] >= testDate]
-    # extract wildfire column as target
-    trainDataY = trainData.pop('WILDFIRE')
-    # Drop Date and ID column
-    trainDataX = trainData.drop(columns=['DATE', 'ID'], axis=1)
-    # specify random over sampling
-    roseSampling = RandomOverSampler(random_state=15)
-    # resample data with specified strategy
-    trainDataX, trainDataY = roseSampling.fit_resample(trainDataX, trainDataY)
-    # extract Wildfire column as testdata target
-    testDataY = testData.pop('WILDFIRE')
-    # Drop Date and ID column
-    testDataX = testData.drop(columns=['DATE', 'ID'], axis=1)
-    # create preprocessing pipeline for numerical and categorical data
-    # create numerical transformer pipeline
-    numericTransformer = Pipeline(steps=[
-        ('imputer', SimpleImputer(strategy='median'))
-    ])
-    # create categorical transformer pipeline
-    categoricTransformer = Pipeline(steps=[
-        ('encoder', OneHotEncoder())
-    ])
-    # select columns with numerical dtypes
-    numericFeatures = trainDataX.select_dtypes(include=['int64', 'float64']).columns
-    # select columns with categorical dtype
-    categoricFeatures = trainDataX.select_dtypes(include=['object']).columns
-    # construct column transformer object to apply pipelines to each column
-    preprocessor = ColumnTransformer(
-        transformers=[
-            ('num', numericTransformer, numericFeatures),
-            ('cat', categoricTransformer, categoricFeatures)],
-        n_jobs=-1, verbose=True)
-    # apply column transformer to train and test data
-    trainDataX = preprocessor.fit_transform(trainDataX)
-    testDataX = preprocessor.fit_transform(testDataX)
-    # return trainData and testdata X and Y dataframe in tuple format
-    return (trainDataX, trainDataY), (testDataX, testDataY)
+class modelPrediction:
+    def __init__(self, validation, dataPath, testDate):
+        # transform datafile path into pathlib object
+        self.dataPath = Path(dataPath)
+        # create directory for use case
+        self.loggingPath = Path('wildfirearea/modelling').joinpath(self.dataPath.stem)
+        self.loggingPath.mkdir(exist_ok=True)
+        # check if input is eligeble for data processing
+        # check if dataPath input is a file
+        assert self.dataPath.is_file(), f'{self.dataPath} does not link to a file'
+        # check if testDate input can be interpreted as date
+        assert not pd.isna(pd.to_datetime(testDate, errors='coerce')), f'{testDate} is not an eligible date'
+        # assign test date as class variable
+        self.testDate = testDate
+        # perform data preprocessing for training and test data
+        trainData, testData = self.dataPreprocess()
+        # perform validation if set to true
+        if validation:
+            parameterSettings = self.parameterTuning(trainData, testData)
+        # if validation set to false empty dict is used
+        else:
+            parameterSettings = {}
+        # perform training based on train and test dataset and parametersettings
+        self.modelTraining(trainData, testData, parameterSettings)
 
-
-def xgboostDef(dataTrain, dataTest):
-    print('XGBoost started')
-    # extract dataframes from train and test data tuples
-    dataTrainX = dataTrain[0]
-    dataTrainY = dataTrain[1]
-    dataTestX = dataTest[0]
-    dataTestY = dataTest[1]
     
-    # specify extra gradient boosting classifier
-    xgbCl = xgb.XGBClassifier(objective="binary:logistic", seed=15, n_jobs=-2)
-    '''
-    specify bayesian search cross validation with the following specifications
-        - estimator: specified extra gradient boosting classifier
-        - search_spaces: defined optimization area for hyperparameter tuning
-        - cv: Specifying split for cross validation with 5 splits
-        - scoring: optimization function based on f1_marco-score optimization
-        - verbose: Output while optimizing
-        - n_jobs: Parallel jobs to be used for optimization using 2 jobs
-        - n_iter: Iteration for optimization
-        - return_train_score: Return training scores for each fold
-    '''
-    cv = BayesSearchCV(estimator=xgbCl,
-                        # specifying search space for 
-                        search_spaces={
-                            'learning_rate': Real(0.01, 1.0, prior='log-uniform'),
-                            'min_child_weight': Real(0, 10, prior='uniform'),
-                            'max_depth': Integer(0, 50, prior='uniform'),
-                            'max_delta_step': Real(0, 20, prior='uniform'),
-                            'subsample': Real(0.01, 1.0, prior='uniform'),
-                            'colsample_bytree': Real(0.01, 1.0, prior='uniform'),
-                            'colsample_bylevel': Real(0.01, 1.0, prior='uniform'),
-                            'reg_lambda': Real(1e-9, 1000, prior='log-uniform'),
-                            'reg_alpha': Real(1e-9, 1.0, 'log-uniform'),
-                            'gamma': Real(1e-9, 0.5, prior='log-uniform'),
-                            'n_estimators': Integer(50, 200, prior='uniform'),
-                            'scale_pos_weight': Real(1e-6, 500, prior='log-uniform')},
-                        cv=5,
-                        scoring='f1_macro',
-                        verbose=3,
-                        n_jobs=1,
-                        error_score='raise',
-                        random_state=14,
-                        n_iter=30,
-                        return_train_score=True)
-    # fit specified cross validation setup to 
-    cv.fit(dataTrainX, dataTrainY)
-    # predict test classes based on test data
-    predClass = cv.predict(dataTestX)
-    # print best parameter combination
-    print(f'Best parameters: {cv.best_params_}')
-    # store best parameter combination in pickle format
-    with open('wildfirearea/modelling/xgboostBestParams.pkl') as f:
-        pickle.dump(cv.best_params_, f)
-    # print detailed results of cross validation
-    print(f'Overall results: {cv.cv_results_}')
-    # store results of cross validation
-    with open('wildfirearea/modelling/xgboostResults.pkl') as f:
-        pickle.dump(cv.cv_results_, f)
-    # print sum of predicted classes to see number of predicted wildfires
-    print(f'Sum of prediction:{sum(predClass)}' )
-    # print classification report
-    print(f'Model score: {classification_report(dataTestY, predClass)}')
+    def dataPreprocess(self):
+        print('Data Preprocessing')
+        # read file into dataframe
+        data = pd.read_csv(self.dataPath)
+        # transform DATE column into datetime
+        data['DATE'] = pd.to_datetime(data['DATE'])
+        # split data into train and testset based on specified date
+        # create train dataframe which is under specified date
+        trainData = data[data['DATE'] < self.testDate]
+        # create test dataframe which is over specified date
+        testData = data[data['DATE'] >= self.testDate]
+        # extract wildfire column as target
+        trainDataY = trainData.pop('WILDFIRE')
+        # Drop Date and ID column
+        trainDataX = trainData.drop(columns=['DATE', 'ID'], axis=1)
+        # specify random over sampling
+        roseSampling = RandomOverSampler(random_state=15)
+        # resample data with specified strategy
+        trainDataX, trainDataY = roseSampling.fit_resample(trainDataX, trainDataY)
+        # extract Wildfire column as testdata target
+        testDataY = testData.pop('WILDFIRE')
+        # Drop Date and ID column
+        testDataX = testData.drop(columns=['DATE', 'ID'], axis=1)
+        # create preprocessing pipeline for numerical and categorical data
+        # create numerical transformer pipeline
+        numericTransformer = Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median'))
+        ])
+        # create categorical transformer pipeline
+        categoricTransformer = Pipeline(steps=[
+            ('encoder', OneHotEncoder())
+        ])
+        # select columns with numerical dtypes
+        numericFeatures = trainDataX.select_dtypes(include=['int64', 'float64']).columns
+        # select columns with categorical dtype
+        categoricFeatures = trainDataX.select_dtypes(include=['object']).columns
+        # construct column transformer object to apply pipelines to each column
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', numericTransformer, numericFeatures),
+                ('cat', categoricTransformer, categoricFeatures)],
+            n_jobs=-1, verbose=True)
+        # apply column transformer to train and test data
+        trainDataX = preprocessor.fit_transform(trainDataX)
+        testDataX = preprocessor.fit_transform(testDataX)
+        # return trainData and testdata X and Y dataframe in tuple format
+        return (trainDataX, trainDataY), (testDataX, testDataY)
+
+
+    def parameterTuning(self, dataTrain, dataTest):
+        print('Parameter tuning')
+        # extract dataframes from train and test data tuples
+        dataTrainX = dataTrain[0]
+        dataTrainY = dataTrain[1]
+        dataTestX = dataTest[0]
+        dataTestY = dataTest[1]
+        
+        # specify extra gradient boosting classifier
+        xgbCl = xgb.XGBClassifier(objective="binary:logistic", seed=15, n_jobs=-2)
+        '''
+        specify bayesian search cross validation with the following specifications
+            - estimator: specified extra gradient boosting classifier
+            - search_spaces: defined optimization area for hyperparameter tuning
+            - cv: Specifying split for cross validation with 5 splits
+            - scoring: optimization function based on f1_marco-score optimization
+            - verbose: Output while optimizing
+            - n_jobs: Parallel jobs to be used for optimization using 2 jobs
+            - n_iter: Iteration for optimization
+            - refit: Set to false as only parameter settings need to be extracted
+        '''
+        cv = BayesSearchCV(estimator=xgbCl,
+                            # specifying search space for 
+                            search_spaces={
+                                'learning_rate': Real(0.01, 1.0, prior='log-uniform'),
+                                'min_child_weight': Real(0, 10, prior='uniform'),
+                                'max_depth': Integer(0, 50, prior='uniform'),
+                                'max_delta_step': Real(0, 20, prior='uniform'),
+                                'subsample': Real(0.01, 1.0, prior='uniform'),
+                                'colsample_bytree': Real(0.01, 1.0, prior='uniform'),
+                                'colsample_bylevel': Real(0.01, 1.0, prior='uniform'),
+                                'reg_lambda': Real(1e-9, 1000, prior='log-uniform'),
+                                'reg_alpha': Real(1e-9, 1.0, 'log-uniform'),
+                                'gamma': Real(1e-9, 0.5, prior='log-uniform'),
+                                'n_estimators': Integer(50, 200, prior='uniform'),
+                                'scale_pos_weight': Real(1e-6, 500, prior='log-uniform')},
+                            cv=5,
+                            scoring='f1_macro',
+                            verbose=3,
+                            n_jobs=1,
+                            error_score='raise',
+                            random_state=14,
+                            n_iter=25,
+                            refit=False)
+        # fit specified cross validation setup to 
+        cv.fit(dataTrainX, dataTrainY)
+        # predict class
+        predClass = cv.predict(dataTestX)
+        # print best parameter combination
+        print(f'Best parameters: {cv.best_params_}')
+        # store best parameter combination in pickle format
+        with open(f'{self.loggingPath}/xgboostBestParams.pkl', 'wb') as f:
+            pickle.dump(cv.best_params_, f)
+        # print detailed results of cross validation
+        print(f'Overall results: {cv.cv_results_}')
+        # store results of cross validation
+        with open(f'{self.loggingPath}/xgboostResults.pkl', 'wb') as f:
+            pickle.dump(cv.cv_results_, f)
+        # print sum of predicted classes to see number of predicted wildfires
+        print(f'Sum of prediction:{sum(predClass)}' )
+        # print classification report
+        print(f'Model score:\n{classification_report(dataTestY, predClass)}')
+        # print confusion matrix of classification
+        print(f'Confusion matrix:\n{confusion_matrix(dataTestY, predClass)}')
+
+    def modelTraining(self, trainData, testData, parameterSettings):
+        print('Model training')
+        # extract dataframes from train and test data tuples
+        dataTrainX = trainData[0]
+        dataTrainY = trainData[1]
+        dataTestX = testData[0]
+        dataTestY = testData[1]
+        # specify extra gradient boosting classifier
+        xgbCl = xgb.XGBClassifier(**parameterSettings, objective="binary:logistic", seed=15, n_jobs=-2)
+        # fit specified model to training data
+        xgbCl.fit(dataTrainX, dataTrainY)
+        # perform prediction on test dataset with trained model
+        predClass = xgbCl.predict(dataTestX)
+        # calculate probability for AUC calculation
+        predProb = xgbCl.predict_proba(dataTestX)[:,1]
+        # print feature importance
+        print(f'Feature imporance:{xgbCl.feature_importances_}')
+        # print confusion matrix of classification
+        print(f'Confusion matrix:\n{confusion_matrix(dataTestY, predClass)}')
+        # print AUC metric
+        print(f'AUC Score:\n{roc_auc_score(dataTestY, predProb)}')
+        # print classification report
+        print(f'Model score:\n{classification_report(dataTestY, predClass)}')
+        
+
 
 if __name__ == '__main__':
-    trainData, testData = dataPreprocess('/home/martinboe/GitHub/wildfirearea/data/usecase/usecase1.csv', '2020-01-01')
-    xgboostDef(trainData, testData)
+    model = modelPrediction(validation=False, dataPath='data/usecase/usecase2.csv', testDate='2020-01-01')
--- wildfirearea/modelling/rdf2vec.py
+++ wildfirearea/modelling/rdf2vec.py
@@ -1,5 +1,6 @@
 # import packages
 import pandas as pd
+import pickle
 from tqdm import tqdm
 from pathlib import Path
 from pyrdf2vec import RDF2VecTransformer
@@ -8,35 +9,66 @@ from pyrdf2vec.graphs import KG, Vertex
 from pyrdf2vec.walkers import RandomWalker
 
 
-def dataPreparation(dataPath):
-    dataPath = Path(dataPath)
-    # Read a CSV file containing the entities we want to classify.
-    graphData = pd.read_csv(dataPath)
-    graphDataList = graphData.values.tolist()
-    entitiesFrom = list(set(graphData['from']))
-    entitiesTo = list(set(graphData['to']))
-    entities = entitiesFrom + entitiesTo
-    return graphDataList, entities
+class kgEmbedding:
 
-def kgCreation(inputGraphList, inputEntities):
-    # initialize knowledge Graph
-    osmKG = KG()
-    # build knowledge graph
-    for row in tqdm(inputGraphList):
-        subj = Vertex(row[0])
-        obj = Vertex(row[1])
-        pred = Vertex(row[2], predicate=True, vprev=subj, vnext=obj)
-        osmKG.add_walk(subj, pred, obj)
-    # build transformer
-    transformer = RDF2VecTransformer(
-        Word2Vec(epochs=10),
-        walkers=[RandomWalker(4, 10, with_reverse=False, n_jobs=-3)],
-        verbose=1
-    )
-    # extract embeddings
-    embeddings, literals = transformer.fit_transform(osmKG, inputEntities)
-    print(literals[:8])
+    def __init__(self, dataPath):
+        self.dataPath = Path(dataPath)
+        trainingDf, transformDf, entities = self.dataPreparation()
+        transformer = self.kgTraining(trainingDf, entities)
+        resultDict = self.kgTransformer(transformer, transformDf, entities)
+        with open('wildfirearea/modelling/KnowledgeGraph', 'wb') as f:
+            pickle.dump(resultDict, f)
+
+
+    def dataPreparation(self):
+        print('Data Preparation')
+        # Read a CSV file containing the entities we want to classify.
+        graphData = pd.read_csv(self.dataPath)
+        trainingDf = graphData.groupby('YEAR')[['from', 'to', 'description']].agg(list)
+        transformDf = graphData.groupby(['YEAR', 'ID'])[['from', 'to', 'description']].agg(list)
+        entitiesFrom = list(set(graphData['from']))
+        entitiesTo = list(set(graphData['to']))
+        entities = list(set(entitiesFrom + entitiesTo))
+        return trainingDf, transformDf, entities
+
+
+    def kgTraining(self, trainingDf, inputEntities):
+        #initalize rdf2vec transformer dictionary
+        transformerDict = {}
+        # build knowledge graph
+        for index, row in tqdm(trainingDf.iterrows()):
+            # initialize knowledge Graph
+            osmKG = KG()
+            rowValues = pd.DataFrame(dict(row)).values.tolist()
+            for row in tqdm(rowValues.iterrows(), leave=False):
+                subj = Vertex(row[0])
+                obj = Vertex(row[1])
+                pred = Vertex(row[2], predicate=True, vprev=subj, vnext=obj)
+                osmKG.add_walk(subj, pred, obj)
+            # build transformer
+            transformer = RDF2VecTransformer(
+                Word2Vec(epochs=20),
+                walkers=[RandomWalker(8, 12, with_reverse=False, n_jobs=-2)],
+                verbose=1
+            )
+            # extract embeddings
+            transformer.fit(osmKG, inputEntities)
+            transformerDict[index] = transformer
+        return transformer
+
+    def kgTransformer(self, transformer, transformDf, inputEntities):
+        resultDict = {}
+        for index, row in tqdm(transformDf.iterrows()):
+            rowValues = pd.DataFrame(dict(row)).values.tolist()
+            osmKG = KG()
+            for row in tqdm(rowValues, leave=False):
+                subj = Vertex(row[0])
+                obj = Vertex(row[1])
+                pred = Vertex(row[2], predicate=True, vprev=subj, vnext=obj)
+                osmKG.add_walk(subj, pred, obj)
+            entities, literals = transformer.transform(osmKG, inputEntities)
+            resultDict[index] = {'entities': entities, 'literals': literals}
+        return resultDict
 
 if __name__ == '__main__':
-    graphDataList, entities = dataPreparation('data/network/openstreetmapGraph.csv')
-    kgCreation(graphDataList, entities)
+    kgEmbedding('data/network/openstreetmapGraph.csv')
