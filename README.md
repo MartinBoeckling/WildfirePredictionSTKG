fac280d4871a9d03698cc3233d392320c861d67b
MartinBoeckling <m_boeck@outlook.de>
2022-06-01 14:53:15 +0000

    Parameterization RDF2Vec and updated README

--- README.md
+++ README.md
@@ -5,10 +5,13 @@
 The coding related to the thesis is implemented in Python and R. For the different programming languages the dependencies are outlined in the following sections.
 
 ### R
-The R coding is based on the R version 4.0.3.
-The dependencies of the project can be found in the [renv.lock](wildfirearea/renv.lock) file. The dependencies can directly be installed in R by using the [renv](https://cran.r-project.org/web/packages/renv/index.html) package. To install all necessary packages simply use the command renv::restore(lockfile='*lockfile path*') within your RStudio environment.
+The R coding is based on the R version 4.1.2.
+The dependencies of the project can be found in the [renv.lock](wildfirearea/renv.lock) file. The dependencies can directly be installed in R by using the [renv](https://cran.r-project.org/web/packages/renv/index.html) package. To install all necessary packages simply use the command renv::restore(lockfile='*lockfile path*') within your R environment.
+
+For using the geospatial packages like sf within R GDAL, GEOS and Proj.4 needs to be installed. Instructions for downloading can be found under the following [link](https://r-spatial.github.io/sf/#installing)
+
 ### Python
-The python coding is based on the python version 3.9.
+The python coding is based on python version 3.9.
 The dependencies can be found in the [requirements.txt](wildfirearea/requirements.txt) file. To install the packages used in this project please run pip install -r requirements.txt in your virtual environement.
 ## Setup for github repo
 
--- wildfirearea/modelling/rdf2vec.py
+++ wildfirearea/modelling/rdf2vec.py
@@ -1,5 +1,27 @@
+'''
+Title:
+RDF2Vec transformation script
+
+Description:
+This script transforms an edge dataframe containing the following columns into a vector
+representation using the RDF2Vec algorithm:
+    - from: ID of grid cell or ID of geometric object as starting node
+    - to: ID of grid cell or ID of geometric object as target node
+    - description: Description of edge relation between starting and target node
+    - YEAR: Year of geometric object
+    - ID: Associated ID of grid cell.
+
+The script contains three main methods. The first method is the dataPreparation method
+which splits the edge dataframe into a training dataframe
+
+Input:
+
+Output:
+
+'''
 # import packages
 import pandas as pd
+import numpy as np
 import pickle
 from tqdm import tqdm
 from pathlib import Path
@@ -12,11 +34,21 @@ from pyrdf2vec.walkers import RandomWalker
 class kgEmbedding:
 
     def __init__(self, dataPath):
+        # transform string to Path structure
         self.dataPath = Path(dataPath)
-        trainingDf, transformDf, entities = self.dataPreparation()
-        transformer = self.kgTraining(trainingDf, entities)
-        resultDict = self.kgTransformer(transformer, transformDf, entities)
-        with open('wildfirearea/modelling/KnowledgeGraph', 'wb') as f:
+        # create logging directory Path name based on file name
+        loggingDirectory = Path(f'wildfirearea/modelling/KnowledgeGraph/{self.dataPath.stem}')
+        # create logging directory
+        loggingDirectory.mkdir(exist_ok=True)
+        # create training and transform dataframe for two differrent phases
+        trainingDf, transformDf = self.dataPreparation()
+        # extract file dictionary containing transformer path
+        fileDict = self.kgTraining(trainingDf, loggingDirectory)
+        # extract result dictionary with ID and corresponding year
+        resultDict = self.kgTransformer(transformDf, fileDict)
+        print('Write result')
+        # store resultDict into pickle file
+        with open(f'{str(loggingDirectory)}/result.pkl', 'wb') as f:
             pickle.dump(resultDict, f)
 
 
@@ -24,50 +56,72 @@ class kgEmbedding:
         print('Data Preparation')
         # Read a CSV file containing the entities we want to classify.
         graphData = pd.read_csv(self.dataPath)
-        trainingDf = graphData.groupby('YEAR')[['from', 'to', 'description']].agg(list)
+        # change all columns to object type
+        graphData = graphData.astype(str)
+        # group graphData to a year stored in list
+        trainingDf = graphData.groupby('YEAR')[['from', 'to', 'description', 'ID']].agg(list)
+        # group graphData to year and ID stored in list
         transformDf = graphData.groupby(['YEAR', 'ID'])[['from', 'to', 'description']].agg(list)
-        entitiesFrom = list(set(graphData['from']))
-        entitiesTo = list(set(graphData['to']))
-        entities = list(set(entitiesFrom + entitiesTo))
-        return trainingDf, transformDf, entities
+        # return trainingDf and transformDf dataframe
+        return trainingDf, transformDf
 
 
-    def kgTraining(self, trainingDf, inputEntities):
+    def kgTraining(self, trainingDf, loggingPath):
+        print('KG Embedding Training')
         #initalize rdf2vec transformer dictionary
         transformerDict = {}
         # build knowledge graph
-        for index, row in tqdm(trainingDf.iterrows()):
-            # initialize knowledge Graph
+        for index, row in tqdm(trainingDf.iterrows(), total=trainingDf.shape[0]):
+            # transform row of type list into dictionary to store into a dataframe with unique rows
+            rowValuesDf = pd.DataFrame(dict(row)).drop_duplicates(ignore_index=True)
+            # extract entities from column from
+            entities = pd.unique(rowValuesDf.pop('ID'))
+            # transform values of row values dataframe into list
+            rowValues = rowValuesDf.values.tolist()
+            # initialize Knowledge Graph
             osmKG = KG()
-            rowValues = pd.DataFrame(dict(row)).values.tolist()
-            for row in tqdm(rowValues.iterrows(), leave=False):
-                subj = Vertex(row[0])
-                obj = Vertex(row[1])
-                pred = Vertex(row[2], predicate=True, vprev=subj, vnext=obj)
+            # create subject, predicate and object and append it to walk
+            for rowValue in tqdm(rowValues, leave=False):
+                subj = Vertex(rowValue[0])
+                obj = Vertex(rowValue[1])
+                pred = Vertex(rowValue[2], predicate=True, vprev=subj, vnext=obj)
                 osmKG.add_walk(subj, pred, obj)
             # build transformer
             transformer = RDF2VecTransformer(
                 Word2Vec(epochs=20),
-                walkers=[RandomWalker(8, 12, with_reverse=False, n_jobs=-2)],
+                walkers=[RandomWalker(14, 36, with_reverse=False, n_jobs=6)],
                 verbose=1
             )
+            # extract walks
+            extractedWalks = transformer.get_walks(osmKG, entities)
             # extract embeddings
-            transformer.fit(osmKG, inputEntities)
-            transformerDict[index] = transformer
-        return transformer
+            transformer.fit(extractedWalks)
+            # save transformer model
+            transformerPath = f'{str(loggingPath)}/transformer{index}.pkl'
+            transformer.save(transformerPath)
+            # append created path to dictionary
+            transformerDict[index] = transformerPath
+        return transformerDict
 
-    def kgTransformer(self, transformer, transformDf, inputEntities):
+    def kgTransformer(self, transformDf, fileDict):
+        print('Transformation started')
         resultDict = {}
-        for index, row in tqdm(transformDf.iterrows()):
-            rowValues = pd.DataFrame(dict(row)).values.tolist()
+        for index, row in tqdm(transformDf.iterrows(), total=transformDf.shape[0]):
+            year = index[0]
+            rowValuesDf = pd.DataFrame(dict(row)).drop_duplicates(ignore_index=True)
+            entity = index[1]
+            rowValues = rowValuesDf.values.tolist()
             osmKG = KG()
-            for row in tqdm(rowValues, leave=False):
-                subj = Vertex(row[0])
-                obj = Vertex(row[1])
-                pred = Vertex(row[2], predicate=True, vprev=subj, vnext=obj)
+             # build transformer
+            fileName = fileDict[year]
+            transformer = RDF2VecTransformer.load(fileName)
+            for rowValue in tqdm(rowValues, leave=False):
+                subj = Vertex(rowValue[0])
+                obj = Vertex(rowValue[1])
+                pred = Vertex(rowValue[2], predicate=True, vprev=subj, vnext=obj)
                 osmKG.add_walk(subj, pred, obj)
-            entities, literals = transformer.transform(osmKG, inputEntities)
-            resultDict[index] = {'entities': entities, 'literals': literals}
+            entitiesVector, literals = transformer.fit_transform(osmKG, entity)
+            resultDict[index] = {'vector': entitiesVector, 'literals': literals}
         return resultDict
 
 if __name__ == '__main__':
